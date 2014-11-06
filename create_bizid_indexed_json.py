d0f1f8c7d15f1067530804f3cab20b21f4a5e715
Michael Woods <woods@yelp.com>
2014-11-06 11:39:19 -0800

    Changed file locations.

--- create_bizid_indexed_json.py
+++ create_bizid_indexed_json.py
@@ -1,8 +1,8 @@
 import ujson as json
 import sys
 
-REVIEW_BIZ_FILE = '/scratch/mariachr/menu_item_extraction/sf_reviews_jan12013_to_nov2014'
-MENU_FILE = '/scratch/mariachr/menu_item_extraction/menutest2'
+REVIEW_BIZ_FILE = '../data/sf_reviews_jan12013_to_nov2014'
+MENU_FILE = '../data/menutest2'
 
 def read_files(review_file, menu_file):
 	
