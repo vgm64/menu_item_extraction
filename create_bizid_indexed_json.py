fbd4e9a4b3c2991232912ce5ee52af6184c61ffa
Michael Woods <woods@yelp.com>
2014-11-06 11:35:02 -0800

    Added function begining before bailing to my machine.

--- create_bizid_indexed_json.py
+++ create_bizid_indexed_json.py
@@ -1,2 +1,8 @@
 import ujson as json
 import sys
+
+REVIEW_BIZ_FILE = '/scratch/mariachr/menu_item_extraction/sf_reviews_jan12013_to_nov2014'
+MENU_FILE = '/scratch/mariachr/menu_item_extraction/menutest2'
+
+def read_files(review_file, menu_file):
+	
