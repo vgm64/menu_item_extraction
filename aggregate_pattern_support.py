c0a7be8df85f777ac6be330dcf39240ac5d45d33
Maria Christoforaki <mariachr@yelp.com>
2014-11-06 17:47:20 -0800

    fixed output format of suffix, prefix phrases

--- aggregate_pattern_support.py
+++ aggregate_pattern_support.py
@@ -31,19 +31,31 @@ def aggregate_pattern_support(filename, outputdir):
         suffixes = sorted(suffixes.items(), key=operator.itemgetter(1),reverse=True)
         out = open(outputdir+"suffix_support", 'w')
         for suffix in suffixes:
-            out.write(suffix[0]+"\t"+str(suffix[1])+"\n")
+            output = dict()
+            output['phrase'] = suffix[0]
+            output['support'] = suffix[1]
+            json.dump(output, out)
+            out.write("\n")
         out.close()
         
         prefixes = sorted(prefixes.items(), key=operator.itemgetter(1),reverse=True)
         out = open(outputdir+"prefix_support", 'w')
         for prefix in prefixes:
-            out.write(prefix[0]+"\t"+str(prefix[1])+"\n")
+            output = dict()
+            output['phrase'] = prefix[0]
+            output['support'] = prefix[1]
+            json.dump(output, out)
+            out.write("\n")
         out.close()
         
         suffix_prefix_pairs = sorted(suffix_prefix_pairs.items(), key=operator.itemgetter(1),reverse=True)
         out = open(outputdir+"suffix_prefix_pair_support", 'w')
         for pair in suffix_prefix_pairs:
-            out.write(pair[0]+"\t"+str(pair[1])+"\n")
+            output = dict()
+            output['phrase'] = pair[0]
+            output['support'] = pair[1]
+            json.dump(output, out)
+            out.write("\n")
         out.close()
 
 aggregate_pattern_support("/scratch/mariachr/menu_item_extraction/data/raw_extracted_patterns","/scratch/mariachr/menu_item_extraction/data/")
